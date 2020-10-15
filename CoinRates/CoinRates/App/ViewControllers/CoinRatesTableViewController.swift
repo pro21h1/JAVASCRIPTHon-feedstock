d396d5569c023c8acc4732f032740e28502062b2
pro21h1 <88898567+pro21h1@users.noreply.github.com>
2020-10-15 16:59:46 +0000

    Initial upload

--- CoinRates/CoinRates/App/ViewControllers/CoinRatesTableViewController.swift
+++ CoinRates/CoinRates/App/ViewControllers/CoinRatesTableViewController.swift
@@ -12,4 +12,10 @@ class CoinRatesTableViewController: UITableViewController {
     
     @available(*, unavailable)
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not be
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        na
