943207f666ad54dd2bdd5399410e68211b1d0f5e
pro21h1 <88898567+pro21h1@users.noreply.github.com>
2020-10-21 20:05:19 +0000

    Update equation src URLs

--- CoinRates/CoinRates/App/ViewControllers/CoinRatesTableViewController.swift
+++ CoinRates/CoinRates/App/ViewControllers/CoinRatesTableViewController.swift
@@ -61,4 +61,7 @@ class CoinRatesTableViewController: UITableViewController {
         let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
         let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
         alertController.addAction(alertAction)
-        self.present(alertController, animated: true, complet
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
+
---
+++ CoinRates/CoinRates/App/Views/CellsRepresentable.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+protocol CoinRateCellRepresentable {
+    var date: String { get }
+    var rate: S
