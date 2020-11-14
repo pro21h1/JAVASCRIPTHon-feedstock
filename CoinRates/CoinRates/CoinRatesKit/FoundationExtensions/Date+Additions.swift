b0d2c7578e9f3e65dce2f11cb73c3be9062cbfc9
pro21h1 <88898567+pro21h1@users.noreply.github.com>
2020-11-14 07:25:51 +0000

    Обновление версии

--- CoinRates/CoinRates/CoinRatesKit/FoundationExtensions/Date+Additions.swift
+++ CoinRates/CoinRates/CoinRatesKit/FoundationExtensions/Date+Additions.swift
@@ -6,4 +6,6 @@ extension Date {
         return Calendar.current.date(byAdding: .day, value: -14, to: self)!
     }
     
-    func formattedDate(from dateString: String) -> Date
+    func formattedDate(from dateString: String) -> Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY-MM-dd
