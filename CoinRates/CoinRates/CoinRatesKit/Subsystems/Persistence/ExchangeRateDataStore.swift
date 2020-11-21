b10b2a31dc1659a86ddc7569a3b1dee0f7ee3c43
pro21h1 <88898567+pro21h1@users.noreply.github.com>
2020-11-21 08:28:21 +0000

    merged with origin

--- CoinRates/CoinRates/CoinRatesKit/Subsystems/Persistence/ExchangeRateDataStore.swift
+++ CoinRates/CoinRates/CoinRatesKit/Subsystems/Persistence/ExchangeRateDataStore.swift
@@ -2,4 +2,5 @@ import Foundation
 
 protocol ExchangeRateDataStore {
     func addExchangeRates(exchangeRates: [ExchangeRate])
-    func fetchExchangeRate
+    func fetchExchangeRates() -> [ExchangeRate]
+}
