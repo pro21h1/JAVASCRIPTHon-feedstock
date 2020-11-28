25d13d55d35d78c9f340ee01be707c44f9991e7c
pro21h1 <88898567+pro21h1@users.noreply.github.com>
2020-11-28 11:58:15 +0000

    Release 3.2.4

--- CoinRates/CoinRates/CoinRatesKit/Subsystems/RemoteAPI/Connection.swift
+++ CoinRates/CoinRates/CoinRatesKit/Subsystems/RemoteAPI/Connection.swift
@@ -1,3 +1,10 @@
 import Foundation
 
-public typealias JSON = Diction
+public typealias JSON = Dictionary<String, Any>
+
+public enum Result<ValueType> {
+    case success(ValueType)
+    case failure(Error)
+    
+    public var value: ValueType? {
+ 
