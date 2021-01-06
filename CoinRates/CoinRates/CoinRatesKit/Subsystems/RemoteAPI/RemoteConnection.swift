fed13fbe5e24d6fefbd712d8ad2d4e032552442e
pro21h1 <88898567+pro21h1@users.noreply.github.com>
2021-01-06 13:41:04 +0000

    optimize some code

--- CoinRates/CoinRates/CoinRatesKit/Subsystems/RemoteAPI/RemoteConnection.swift
+++ CoinRates/CoinRates/CoinRatesKit/Subsystems/RemoteAPI/RemoteConnection.swift
@@ -4,4 +4,5 @@ class RemoteConnection: Connection {
     
     // MARK: Public Interfaces
     func fetchHistoricalRate(forCurrency currency: Currency, from startDate: Date, to endDate: Date, completion: @escaping (Result<JSON>) -> ()) {
-        guard let historicalRatesURL = bitcoinRatesURLWithCompone
+        guard let historicalRatesURL = bitcoinRatesURLWithComponents(currency: currency, from: startDate, to: endDate) else {
+            return completion(.failure(CoinR
