37d26aded4de4505dc91b2c98ba51467d305ea4c
pro21h1 <88898567+pro21h1@users.noreply.github.com>
2023-04-20 05:59:41 +0000

    create new file

--- CoinRates/CoinRatesWatchApp/CoinRatesInterfaceController.swift
+++ CoinRates/CoinRatesWatchApp/CoinRatesInterfaceController.swift
@@ -18,4 +18,5 @@ class CoinRatesInterfaceController: WKInterfaceController {
                 switch result {
                 case .success(let exchangeRates):
                     self.exchangeRatesTable.setNumberOfRows(exchangeRates.count, withRowType: "ExchangeRow")
-                    for index in 0..<self.exc
+                    for index in 0..<self.exchangeRatesTable.numberOfRows {
+                        if let controller = self.exchangeRatesTable.rowController(at: index) as? C
