70bfce9ac9e10dd3709c8075f6f0e305b27606aa
pro21h1 <88898567+pro21h1@users.noreply.github.com>
2023-04-22 06:35:51 +0000

    Closes #20

--- CoinRates/CoinRatesWatchApp/CoinRatesRowController.swift
+++ CoinRates/CoinRatesWatchApp/CoinRatesRowController.swift
@@ -1,4 +1,7 @@
 import WatchKit
 
 public class CoinRatesRowController: NSObject {
+    @IBOutlet var dateLabel: WKInterfaceLabel!
+    @IBOutlet var rateLabel: WKInterfaceLabel!
     
+    var exchangeRate: ExchangeRate
