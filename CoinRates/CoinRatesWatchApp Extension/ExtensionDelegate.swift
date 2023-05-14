cbd64ec9fa1c8745b72a13e802f0917ec5cfa6fe
pro21h1 <88898567+pro21h1@users.noreply.github.com>
2023-05-14 17:32:35 +0000

    Cleans up code

--- CoinRates/CoinRatesWatchApp Extension/ExtensionDelegate.swift
+++ CoinRates/CoinRatesWatchApp Extension/ExtensionDelegate.swift
@@ -2,4 +2,10 @@
 //  ExtensionDelegate.swift
 //  CoinRatesWatchApp Extension
 //
-//  Created by Emanuel Mu
+//  Created by Emanuel Munteanu on 30.07.17.
+//  Copyright © 2017 Emanuel Munteanu. All rights reserved.
+//
+
+import WatchKit
+
+class ExtensionD
