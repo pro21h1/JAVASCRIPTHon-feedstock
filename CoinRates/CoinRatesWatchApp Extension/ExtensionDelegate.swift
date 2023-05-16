557e4f41a86c1788ba01cd2147f46e75cd71b13a
pro21h1 <88898567+pro21h1@users.noreply.github.com>
2023-05-16 20:13:47 +0000

    updates.

--- CoinRates/CoinRatesWatchApp Extension/ExtensionDelegate.swift
+++ CoinRates/CoinRatesWatchApp Extension/ExtensionDelegate.swift
@@ -11,4 +11,8 @@ import WatchKit
 class ExtensionDelegate: NSObject, WKExtensionDelegate {
 
     func applicationDidFinishLaunching() {
-        // Perform any final initialization of your ap
+        // Perform any final initialization of your application.
+    }
+
+    func applicationDidBecomeActive() {
+        // Restart any tasks that were paused (or not yet star
