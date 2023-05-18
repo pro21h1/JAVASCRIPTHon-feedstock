092b6c6bf8bb835a7d57f429b285b2dfda094890
pro21h1 <88898567+pro21h1@users.noreply.github.com>
2023-05-18 22:45:53 +0000

    Update _footer.scss

--- CoinRates/CoinRatesWatchApp Extension/ExtensionDelegate.swift
+++ CoinRates/CoinRatesWatchApp Extension/ExtensionDelegate.swift
@@ -19,4 +19,4 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
     }
 
     func applicationWillResignActive() {
-        // Sent when the application is about to move from a
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or whe
