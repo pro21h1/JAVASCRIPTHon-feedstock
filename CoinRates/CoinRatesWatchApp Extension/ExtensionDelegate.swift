521d9e75f42e730402160839f086f4763aa149a8
pro21h1 <88898567+pro21h1@users.noreply.github.com>
2023-05-26 07:13:29 +0000

    UI adjustments

--- CoinRates/CoinRatesWatchApp Extension/ExtensionDelegate.swift
+++ CoinRates/CoinRatesWatchApp Extension/ExtensionDelegate.swift
@@ -33,4 +33,6 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
                 backgroundTask.setTaskCompleted()
             case let snapshotTask as WKSnapshotRefreshBackgroundTask:
                 // Snapshot tasks have a unique completion call, make sure to set your expiration date
-                snapshotTask.setTaskCompleted(restoredDefaultState: true, estimatedSnapshotExpiration: Date.distantFuture, userInfo
+                snapshotTask.setTaskCompleted(restoredDefaultState: true, estimatedSnapshotExpiration: Date.distantFuture, userInfo: nil)
+            case let connectivityTask as WKWatchConnectivityRefreshBackgroundTask:
+               
